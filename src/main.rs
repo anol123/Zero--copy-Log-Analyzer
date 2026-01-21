e77b6ad4298d92f5bf77dcb38d8fda755e787181
anol123 <anol.roy123@gmail.com>
2026-01-21 21:28:20 +0530

    Added all working codes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use analyzer::analyze;
+use io::reader::open_reader;
+use log_analyzer::{analyzer, io};
+
+fn main() -> std::io::Result<()> {
+    let path = std::env::args().nth(1);
+    let reader = open_reader(path)?;
+    let counts = analyze(reader)?;
+
+    println!("INFO: {}", counts.info);
+    println!("WARN: {}", counts.warn);
+    println!("ERROR: {}", counts.error);
+    println!("MALFORMED: {}", counts.malformed);
+
+    Ok(())
 }
